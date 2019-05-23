72654ff2acd9c1e1740b04c7ad3f66a5ca85c940
Martin Tournoij <martin@arp242.net>
2019-05-24 02:42:59 +1200

    Incomplete CSP

--- headers.go
+++ headers.go
@@ -7,7 +7,7 @@ func Headers(next http.Handler) http.Handler {
 		// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 		// TODO: Don't load gfonts, use proper cfg.Static, get rid of
 		// unsafe-inline
-		w.Header().Set("Content-Security-Policy", "default-src localhost:8081; style-src localhost:8081 https://fonts.googleapis.com 'unsafe-inline'; font-src localhost:8081 https://fonts.gstatic.com;")
+		//w.Header().Set("Content-Security-Policy", "default-src localhost:8081; style-src localhost:8081 https://fonts.googleapis.com 'unsafe-inline'; font-src localhost:8081 https://fonts.gstatic.com;")
 		w.Header().Set("Strict-Transport-Security", "max-age=2592000")
 		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
