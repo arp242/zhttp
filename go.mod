b59356c25911479923e67965fd239584fc53addc
Martin Tournoij <martin@arp242.net>
2020-02-21 14:57:50 +0800

    Allow sending emails without a relay

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/monoculum/formam v0.0.0-20191229172733-952f0766a724
 	github.com/pkg/errors v0.9.1
 	github.com/teamwork/guru v1.0.0
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	zgo.at/utils v1.3.1 // indirect
 	zgo.at/zlog v1.0.9
 	zgo.at/ztest v1.0.1
--- zmail/zmail.go
+++ zmail/zmail.go
@@ -3,6 +3,7 @@ package zmail
 
 import (
 	"crypto/rand"
+	"crypto/tls"
 	"fmt"
 	"hash/fnv"
 	"math/big"
@@ -12,16 +13,16 @@ import (
 	"net/mail"
 	"net/smtp"
 	"net/url"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/pkg/errors"
+	"golang.org/x/sync/singleflight"
 	"zgo.at/zlog"
 )
-
-var reNL = regexp.MustCompile(`(\r\n){2,}`)
 
 var (
 	SMTP  = ""   // SMTP server connection string.
@@ -31,15 +32,130 @@ var (
 // Send an email.
 func Send(subject string, from mail.Address, to []mail.Address, body string) error {
 	msg := format(subject, from, to, body)
-
-	if SMTP == "" && Print {
-		l := strings.Repeat("═", 50)
-		fmt.Println("╔═══ EMAIL " + l + "\n║ " +
-			strings.Replace(strings.TrimSpace(string(msg)), "\r\n", "\r\n║ ", -1) +
-			"\n╚══════════" + l + "\n")
-		return nil
+	toList := make([]string, len(to))
+	for i := range to {
+		toList[i] = to[i].Address
 	}
 
+	switch SMTP {
+	case "stdout":
+		if Print {
+			l := strings.Repeat("═", 50)
+			fmt.Println("╔═══ EMAIL " + l + "\n║ " +
+				strings.Replace(strings.TrimSpace(string(msg)), "\r\n", "\r\n║ ", -1) +
+				"\n╚══════════" + l + "\n")
+		}
+		return nil
+	case "":
+		return sendMail(subject, from, toList, msg)
+	default:
+		return sendRelay(subject, from, toList, msg)
+	}
+}
+
+var hostname singleflight.Group
+
+// Send direct.
+func sendMail(subject string, from mail.Address, to []string, body []byte) error {
+	x, _, _ := hostname.Do("hostname", func() (interface{}, error) {
+		return os.Hostname()
+	})
+	hello := x.(string)
+
+	go func() {
+		for _, t := range to {
+			domain := t[strings.LastIndex(t, "@")+1:]
+			mxs, err := net.LookupMX(domain)
+			if err != nil {
+				zlog.Field("domain", domain).Errorf("zmail sendMail: %s", err)
+				return
+			}
+
+			for _, mx := range mxs {
+				logerr := func(err error) {
+					zlog.Fields(zlog.F{
+						"host": mx.Host,
+						"from": from,
+						"to":   to,
+					}).Error(err)
+				}
+
+				c, err := smtp.Dial(mx.Host + ":25")
+				if err != nil {
+					logerr(err)
+					if strings.Contains(err.Error(), " blocked ") {
+						// 14:52:24 ERROR: 554 5.7.1 Service unavailable; Client host [xxx.xxx.xx.xx] blocked using
+						// xbl.spamhaus.org.rbl.local; https://www.spamhaus.org/query/ip/xxx.xxx.xx.xx
+						break
+					}
+					continue // Can't connect: try next MX
+				}
+				defer c.Close()
+
+				err = c.Hello(hello)
+				if err != nil {
+					logerr(err)
+					// Errors from here on are probably fatal error, so just
+					// abort.
+					// TODO: could improve by checking the status code, but
+					// net/smtp doesn't provide them in a good way. This is fine
+					// for now as it's intended as a simple backup solution.
+					break
+				}
+
+				if ok, _ := c.Extension("STARTTLS"); ok {
+					err := c.StartTLS(&tls.Config{ServerName: mx.Host})
+					if err != nil {
+						logerr(err)
+						break
+					}
+				}
+
+				err = c.Mail(from.Address)
+				if err != nil {
+					logerr(err)
+					break
+				}
+				for _, addr := range to {
+					err = c.Rcpt(addr)
+					if err != nil {
+						logerr(err)
+						break
+					}
+				}
+
+				w, err := c.Data()
+				if err != nil {
+					logerr(err)
+					break
+				}
+				_, err = w.Write(body)
+				if err != nil {
+					logerr(err)
+					break
+				}
+
+				err = w.Close()
+				if err != nil {
+					logerr(err)
+					break
+				}
+
+				err = c.Quit()
+				if err != nil {
+					logerr(err)
+					break
+				}
+
+				break
+			}
+		}
+	}()
+	return nil
+}
+
+// Send via relay.
+func sendRelay(subject string, from mail.Address, to []string, body []byte) error {
 	srv, err := url.Parse(SMTP)
 	if err != nil {
 		return err
@@ -52,28 +168,25 @@ func Send(subject string, from mail.Address, to []mail.Address, body string) err
 		host = h
 	}
 
-	toList := make([]string, len(to))
-	for i := range to {
-		toList[i] = to[i].Address
-	}
-
 	go func() {
 		var auth smtp.Auth
 		if user != "" {
 			auth = smtp.PlainAuth("", user, pw, host)
 		}
 
-		err := smtp.SendMail(srv.Host, auth, from.Address, toList, msg)
+		err := smtp.SendMail(srv.Host, auth, from.Address, to, body)
 		if err != nil {
 			zlog.Fields(zlog.F{
 				"host": srv.Host,
 				"from": from,
-				"to":   "toList",
+				"to":   to,
 			}).Error(errors.Wrap(err, "smtp.SendMail"))
 		}
 	}()
 	return nil
 }
+
+var reNL = regexp.MustCompile(`(\r\n){2,}`)
 
 // format a message.
 func format(subject string, from mail.Address, to []mail.Address, body string) []byte {
