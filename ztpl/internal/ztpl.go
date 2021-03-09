1e6c6fdc55b63c56b49eb8d439f106921bcbea60
Martin Tournoij <martin@arp242.net>
2021-03-10 02:18:59 +0800

    Add ztpl.TestTemplateExecution() and related functions to test template execution

--- ztpl/internal/ztpl.go
+++ ztpl/internal/ztpl.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"html/template"
 	"io"
+	"sort"
 	"sync"
+	"text/template/parse"
 )
 
 var Templates = new(LockedTpl)
@@ -31,11 +33,89 @@ func (t *LockedTpl) Has(name string) bool {
 	return t.t.Lookup(name) != nil
 }
 
+func (t *LockedTpl) List() []string {
+	t.Lock()
+	defer t.Unlock()
+	var (
+		tpl = t.t.Templates()
+		l   = make([]string, 0, len(tpl))
+	)
+	for _, a := range tpl {
+		l = append(l, a.Name())
+	}
+	return l
+}
+
+type Trace map[string][]interface{}
+
+func (t Trace) Names() []string {
+	names := make([]string, 0, len(t))
+	for tt := range t {
+		names = append(names, tt)
+	}
+	sort.Strings(names)
+	return names
+}
+
+var (
+	TraceEnabled bool
+	Ran          Trace
+)
+
 func (t *LockedTpl) ExecuteTemplate(wr io.Writer, name string, data interface{}) error {
 	t.Lock()
 	defer t.Unlock()
 	if t.t == nil {
 		return errors.New("ztpl.ExecuteTemplate: not initialized; call ztpl.Init()")
 	}
+
+	if TraceEnabled {
+		Ran[name] = append(Ran[name], data)
+
+		invoks := make(map[string]struct{})
+		for _, n := range t.t.Lookup(name).Tree.Root.Nodes {
+			for _, k := range lsTpl(t.t, n) {
+				invoks[k] = struct{}{}
+			}
+		}
+		for k := range invoks {
+			// TODO: need to process TemplateNode.Pipe to get the data
+			Ran[k] = append(Ran[k], nil)
+		}
+	}
+
 	return t.t.ExecuteTemplate(wr, name, data)
+}
+
+func lsTpl(t *template.Template, n parse.Node) []string {
+	var (
+		tpls  []string
+		nodes []parse.Node
+	)
+	switch nn := n.(type) {
+	case *parse.TemplateNode:
+		tpls = append(tpls, nn.Name)
+		for _, n2 := range t.Lookup(nn.Name).Tree.Root.Nodes {
+			tpls = append(tpls, lsTpl(t, n2)...)
+		}
+	case *parse.IfNode:
+		if nn.List != nil {
+			nodes = append(nodes, nn.List.Nodes...)
+		}
+		if nn.ElseList != nil {
+			nodes = append(nodes, nn.ElseList.Nodes...)
+		}
+	case *parse.ListNode:
+		nodes = nn.Nodes
+	case *parse.CommandNode:
+		nodes = nn.Args
+	case *parse.PipeNode:
+		for _, n2 := range nn.Cmds {
+			nodes = append(nodes, n2.Args...)
+		}
+	}
+	for _, n2 := range nodes {
+		tpls = append(tpls, lsTpl(t, n2)...)
+	}
+	return tpls
 }
--- ztpl/ztpl.go
+++ ztpl/ztpl.go
@@ -3,13 +3,19 @@ package ztpl
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"io"
 	"io/fs"
 	"os"
+	"sort"
+	"strings"
+	"testing"
 
 	"zgo.at/zhttp/ztpl/internal"
 	"zgo.at/zhttp/ztpl/tplfunc"
+	"zgo.at/zstd/zruntime"
+	"zgo.at/zstd/zstring"
 )
 
 // Init sets up the templates.
@@ -33,8 +39,88 @@ func Reload(path string) {
 // IsLoaded reports if templates have been loaded.
 func IsLoaded() bool { return internal.Templates != nil }
 
+// List all template names.
+func List() []string {
+	if internal.Templates == nil {
+		return nil
+	}
+	return internal.Templates.List()
+}
+
+// Trace enables tracking of all template executions. When this is disabled the
+// stored list is emptied and the previous value returned.
+//
+// This is mostly useful for tracking which templates are run from tests, e.g.:
+//
+//   func TestMain(m *testing.M) {
+//       ztpl.Trace(true)
+//       c := m.Run()
+//
+//       ran := ztpl.Trace(false)
+//       for _, t := range ztpl.List() {
+//           if _, ok := ran[t]; !ok {
+//               fmt.Println("didn't execute template", t)
+//           }
+//       }
+//       os.Exit(c)
+//   }
+//
+// Also see TestTemplateExecution(), which wraps this for convenient use.
+func Trace(on bool) internal.Trace {
+	r := internal.Ran
+	internal.TraceEnabled = on
+	internal.Ran = make(internal.Trace)
+	return r
+}
+
+// TestTemplateExecution tests if all templates loaded through ztpl are
+// executed.
+//
+// Go templates are dynamically typed and not counted in code coverage tools;
+// this is a simple way to ensure all templates are executed at least once.
+//
+// Typical usage would be:
+//
+//   func TestMain(m *testing.M) {
+//       os.Exit(ztpl.TestTemplateExecution(m, "ignore_this.gohtml"))
+//   }
+func TestTemplateExecution(m *testing.M, ignore ...string) int {
+	Trace(true)
+	c := m.Run()
+
+	// Don't run if tests failed or if -run was given; it's likely to cause
+	// failures anyway so isn't all that useful.
+	//
+	// TODO: provide option to list which templates were executed when -run is
+	// given; maybe through env? Can be useful for testing and seeing which
+	// templates are being run.
+	if c > 1 {
+		return c
+	}
+	for _, a := range os.Args {
+		if strings.HasPrefix(a, "-test.run") || strings.HasPrefix(a, "-test.bench") {
+			return c
+		}
+	}
+
+	ran := Trace(false).Names()
+	unrun := sort.StringSlice(zstring.Difference(List(), ran, ignore))
+	if len(unrun) == 0 {
+		return 0
+	}
+
+	fmt.Fprintln(os.Stderr, "    --- FAIL: ztpl.TestTemplateExecution")
+	fmt.Fprintf(os.Stderr, "\t\tDidn't execute the templates:\n\t\t\t%s\n", strings.Join(unrun, "\n\t\t\t"))
+	if zruntime.TestVerbose() {
+		fmt.Fprintf(os.Stderr, "\n\t\tTemplates executed: %s\n\n", ran)
+	}
+	return 1
+}
+
 // HasTemplate reports if this template is loaded.
-func HasTemplate(name string) bool { return internal.Templates != nil && internal.Templates.Has(name) }
+func HasTemplate(name string) bool {
+	return internal.Templates != nil && internal.Templates.Has(name)
+}
 
 // Execute a named template.
 func Execute(w io.Writer, name string, data interface{}) error {
