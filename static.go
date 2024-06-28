302cde73fc4797e483a65a263b38765aae1c207c
Martin Tournoij <martin@arp242.net>
2024-06-28 13:43:19 +0100

    Fall back to "/index.html" for "/"

--- static.go
+++ static.go
@@ -69,6 +69,9 @@ func (s Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	path := strings.TrimLeft(r.URL.Path, "/")
+	if path == "" {
+		path = "index.html"
+	}
 	d, err := fs.ReadFile(s.files, path)
 	if err != nil {
 		if os.IsNotExist(err) {
